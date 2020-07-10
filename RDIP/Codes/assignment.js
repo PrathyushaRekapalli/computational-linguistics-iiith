9b16c9048b8272cdc0d201ad9eae99c2105ddf3c
PrathyushaRekapalli <prathyu.rekapalli@gmail.com>
2020-07-10 21:45:06 +0530

    Goal3 submitted

---
+++ RDIP/Codes/assignment.js
@@ -0,0 +1,5 @@
+function select_language()
+{
+	document.getElementById('demo').innerHTML="Form a sentence (Declarative or Interrogative or any other type) from the given words";
+	document.getElementById('demo1').innerHTML="(select the buttons in proper order)";
+}
